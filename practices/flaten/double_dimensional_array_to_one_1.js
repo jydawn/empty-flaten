461d6f4ac612098c7436c93cdc091984968ebf6e
jydawn <1198621249@qq.com>
2016-06-14 17:26:01 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,13 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var temp=[];
+  for(var i=0;i<collection.length;i++)
+    for(var j=0;j<collection[i].length;j++)
+    {
+      temp.push(collection[i][j]);
+    }
+    return temp;
 }
 
 module.exports = double_to_one;
